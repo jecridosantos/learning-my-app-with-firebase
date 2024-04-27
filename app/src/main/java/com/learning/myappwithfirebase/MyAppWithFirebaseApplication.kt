00ccf17bceba0038d43d2b50fddc5e71a0223515
Jecri Do Santos <jreydosantos@proveedor.intercorp.com.pe>
2024-04-27 09:24:09 -0500

    configure application for dagger

---
+++ app/src/main/java/com/learning/myappwithfirebase/MyAppWithFirebaseApplication.kt
@@ -0,0 +1,8 @@
+package com.learning.myappwithfirebase
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyAppWithFirebaseApplication: Application() {
+}
